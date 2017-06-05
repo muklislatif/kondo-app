a30d6a9d2b93d159fd72934f9aeea4871d40936d
Achmad Mahardi <achmad@mahardi.me>
2017-06-06 00:52:00 +0700

    increase kill timeout to 8s * fix pm2 config

--- server/server.config.js
+++ server/server.config.js
@@ -3,12 +3,15 @@ const fs = require('fs');
 const { name, env, env_production } = JSON.parse(fs.readFileSync(`${__dirname}/config.json`, 'utf8'));
 
 module.exports = {
-  name,
-  env,
-  env_production,
-  script: './server.js',
-  cwd: './',
-  instances: 'max',
-  exec_mode: 'cluster',
-  merge_logs: true,
+  apps: [{
+    name,
+    env,
+    env_production,
+    script: './server.js',
+    cwd: './',
+    instances: 'max',
+    exec_mode: 'cluster',
+    merge_logs: true,
+    kill_timeout: 8000,
+  }],
 };
