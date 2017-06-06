86443a41e3507a84541291e4f32665f7cbcc5bf8
Achmad Mahardi <maman@users.noreply.github.com>
2017-06-06 20:46:19 +0700

    fix migration (#5)

--- server/migration/initiate.js
+++ server/migration/initiate.js
@@ -6,27 +6,28 @@ const { logger } = require('../utils/logger');
 
 const migrationScript = fs.readFileSync(`${__dirname}/schema.sql`, 'utf8').split('====');
 
-const executeSQL = query => db
-  .getConnection(conn => conn.query(query))
-  .then(() => logger.info(`DONE: ${query}`));
+const executeSQL = (query, idx, length) => db
+  .getConnection()
+  .then(conn => conn.query(query))
+  .then(() => logger.info(`[MIGRATION] [${1 + idx}/${length}]: ${query.split('\n').join(' ')}`))
+  .catch(err => logger.error(`[MIGRATION] [${1 + idx}/${length}]: `, err));
 
 const migrate = () => {
-  logger.info('Initiating migration...');
+  logger.info('[MIGRATION] Initiating migration...');
   execSync(`mysql --user=${process.env.MYSQL_USER} --password=${process.env.MYSQL_PASSWORD} --execute="CREATE SCHEMA IF NOT EXISTS ${process.env.MYSQL_DBNAME}"`);
-  logger.info('Using the following SQL:');
-  logger.info(migrationScript);
+  logger.info('[MIGRATION] Using the following SQL:');
   return Promise.mapSeries(migrationScript, executeSQL)
     .then(() => {
-      logger.info('Done initiating db');
+      logger.info('[MIGRATION] Done initiating db');
     });
 };
 
 migrate()
   .then(() => {
-    logger.info('Please ignore error message from nodemon, the migration has passed successfully.');
+    logger.info('[MIGRATION] Please ignore error message from nodemon, the migration has passed successfully.');
   })
   .catch((err) => {
-    logger.error(err);
+    logger.error('[MIGRATION] Cannot migrate db: ', err);
   })
   .finally(() => {
     process.kill(process.pid);
