aeaec7d56b918fd9965269d85e6f7e1003b95169
Arian Pradana <arian.raptor@gmail.com>
2017-06-14 12:57:10 +0700

    Split image resize

--- client/src/views/frontoffice/components/Carousel/Carousel.jsx
+++ client/src/views/frontoffice/components/Carousel/Carousel.jsx
@@ -1,45 +1,86 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Slider from 'react-slick';
+import root from 'window-or-global';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './Carousel.css';
 
-const Carousel = () => {
-  const settings = {
-    lazyLoad: 'progressive',
-    slidesToShow: 1,
-    dots: true,
-    arrows: false,
-    infinite: true,
-    speed: 500,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    pauseOnHover: false,
-    centerPadding: 0,
-    centerMode: true,
-  };
+class Carousel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMobile: true,
+    };
+  }
 
-  return (
-    <div className="carousel clearfix relative">
-      <a className="crs-brand" href="/">
-          Kondo APP
-      </a>
-      <div className="crs-inner">
-        <Slider {...settings}>
-          <a href="/" className="slick-item">
-            <img className="crs-img img-responsive" src="http://placehold.it/1575x675" alt="Lorem ipsum" />
-          </a>
-          <a href="/" className="slick-item">
-            <img className="crs-img img-responsive" src="http://placehold.it/1575x675" alt="Lorem ipsum" />
-          </a>
-          <a href="/" className="slick-item">
-            <img className="crs-img img-responsive" src="http://placehold.it/1575x675" alt="Lorem ipsum" />
-          </a>
-        </Slider>
+  componentDidMount() {
+    this.updateDimensions();
+    root.addEventListener('resize', this.updateDimensions.bind(this));
+  }
+
+  componentWillUnmount() {
+    root.removeEventListener('resize', this.updateDimensions.bind(this));
+  }
+
+  updateDimensions() {
+    const width = root.outerWidth;
+    this.setState({
+      isMobile: width <= 870,
+    });
+  }
+
+  render() {
+    const settings = {
+      lazyLoad: 'progressive',
+      slidesToShow: 1,
+      dots: true,
+      arrows: false,
+      infinite: true,
+      speed: 500,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      pauseOnHover: false,
+      centerPadding: 0,
+      centerMode: true,
+    };
+
+    const imgMobile = 'http://placehold.it/870x300';
+    const imgDesktop = 'http://placehold.it/1920x376';
+
+    return (
+      <div className="carousel clearfix relative">
+        <a className="crs-brand" href="/">
+            Kondo APP
+        </a>
+        <div className="crs-inner">
+          <Slider {...settings}>
+            <a href="/" className="slick-item">
+              <img
+                className="crs-img img-responsive"
+                src={this.state.isMobile ? imgMobile : imgDesktop}
+                alt="Lorem ipsum"
+              />
+            </a>
+            <a href="/" className="slick-item">
+              <img
+                className="crs-img img-responsive"
+                src={this.state.isMobile ? imgMobile : imgDesktop}
+                alt="Lorem ipsum"
+              />
+            </a>
+            <a href="/" className="slick-item">
+              <img
+                className="crs-img img-responsive"
+                src={this.state.isMobile ? imgMobile : imgDesktop}
+                alt="Lorem ipsum"
+              />
+            </a>
+          </Slider>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Carousel;
