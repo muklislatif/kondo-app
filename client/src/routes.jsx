6c10dbe1514836f994aa76d7ce54d291861e140e
Arian Pradana <arian.raptor@gmail.com>
2017-06-14 14:16:40 +0700

    Revert "Transition"
    This reverts commit 50fe68d2b90cfb0310309fd981f7b1a6fbd69e4d.

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -4,7 +4,6 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
-import { RouteTransition } from 'react-router-transition';
 import createBrowserHistory from 'history/createBrowserHistory';
 
 // Front Office
@@ -26,33 +25,24 @@ const history = createBrowserHistory({
 
 const Routes = () => (
   <Router history={history}>
-    <Route
-      render={({ location, history, match }) => {
-        return (
-          <RouteTransition
-            pathname={location.pathname}
-            atEnter={{ opacity: 0 }}
-            atLeave={{ opacity: 0 }}
-            atActive={{ opacity: 1 }}
-          >
-            <Switch key={location.key} location={location}>
-              <Route exact path="/" component={Home} />
-              <Route path="/community-feed" component={CommunityFeed} />
-              <Route path="/add-post" component={PostForm} />
-              <Route exact path="/help-desk" component={HelpDesk} />
-              <Route path="/help-desk/:id" component={HelpDeskDetail} />
-              <Route path="/add-issue" component={IssueForm} />
-              <Route exact path="/service-providers" component={ServiceProvider} />
-              <Route path="/service-providers/:id" component={ServiceProviderList} />
-              <Route exact path="/admin" component={Dashboard} />
-              <Route path="/admin/members" component={Dashboard} />
-              <Route path="/admin/carousels" component={Dashboard} />
-              <Route path="/admin/posts" component={Dashboard} />
-            </Switch>
-          </RouteTransition>
-        )
-      }}
-    />
+    <div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/community-feed" component={CommunityFeed} />
+        <Route path="/add-post" component={PostForm} />
+        <Route exact path="/help-desk" component={HelpDesk} />
+        <Route path="/help-desk/:id" component={HelpDeskDetail} />
+        <Route path="/add-issue" component={IssueForm} />
+        <Route exact path="/service-providers" component={ServiceProvider} />
+        <Route path="/service-providers/:id" component={ServiceProviderList} />
+      </Switch>
+      <Switch>
+        <Route exact path="/admin" component={Dashboard} />
+        <Route path="/admin/members" component={Dashboard} />
+        <Route path="/admin/carousels" component={Dashboard} />
+        <Route path="/admin/posts" component={Dashboard} />
+      </Switch>
+    </div>
   </Router>
 );
 
